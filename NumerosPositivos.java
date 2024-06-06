150efcc5edff7e492dc8b6a62e8af2a61b14a7d8
Julia0126 <42453650+Julia0126@users.noreply.github.com>
2024-06-06 15:29:01 -0300

    Create NumerosPositivos.java

---
+++ NumerosPositivos.java
@@ -0,0 +1,27 @@
+package atividades;
+
+import java.util.Scanner;
+
+public class NumerosPositivos {
+
+	public static void main(String[] args) {
+		Scanner leia = new Scanner(System.in);
+
+		int numero;
+		int soma = 0;
+
+		do {
+			System.out.print("Digite um número: ");
+			numero = leia.nextInt();
+
+			if (numero > 0) {
+				soma = soma + numero;
+			}
+		} while (numero != 0);
+
+		System.out.println("\nA soma dos números positivos é: " + soma);
+
+		leia.close();
+	}
+
+}
